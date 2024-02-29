4c2a828ae6fb244dece8eaf027d3f1629d62128a
vishalrk1 <karangalevr@gmail.com>
2024-02-29 21:43:22 +0530

    optimizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "./index.css";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/HomdPage/Homepage";
 import LoginPage from "./pages/authentication/LoginPage";
 import SignupPage from "./pages/authentication/SignupPage";
-
-import "./App.css";
 import ContactPage from "./pages/Contact/contact";
+
+import "./index.css";
+import "./App.css";
 
 function App() {
   return (
