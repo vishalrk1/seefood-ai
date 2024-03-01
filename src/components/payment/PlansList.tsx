af9935ab8a6cb680c5445d3f557300067d74bf4e
vishalrk1 <karangalevr@gmail.com>
2024-03-01 22:48:43 +0530

    adding new paid plan

--- src/components/payment/PlansList.tsx
+++ src/components/payment/PlansList.tsx
@@ -8,12 +8,13 @@ const PlansList = () => {
       className="mt-5"
       onValueChange={(value) => console.log(value)}
     >
-      <PlanCard credits="80" price="40.00" perCredit="0.50" planName="plan-1" />
+      <PlanCard credits="80" price="40.0" perCredit="0.50" planName="plan-1" />
+      <PlanCard credits="200" price="90.0" perCredit="0.45" planName="plan-2" />
       <PlanCard
-        credits="200"
-        price="90.00"
-        perCredit="0.45"
-        planName="plan-2"
+        credits="500"
+        price="150.0"
+        perCredit="0.30"
+        planName="plan-3"
       />
     </RadioGroup>
   );
